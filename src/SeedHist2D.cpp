17c59625ecb2ea773d5a0f728736b6fac6dab0c5
jvo203 <chris.zapart@nao.ac.jp>
2019-12-12 11:23:49 +0900

    modified:   src/SeedHist2D.cpp

--- src/SeedHist2D.cpp
+++ src/SeedHist2D.cpp
@@ -11,4 +11,35 @@ void SeedH2::update(float _x, float _y) {
     this->flush();
 }
 
-void SeedH2::flush() { init_done = true; }
+void SeedH2::flush() {
+  if (data.size() == 0)
+    return;
+
+  init_done = true;
+
+  double meanX = 0.0;
+  double meanY = 0.0;
+
+  // gather the statistics
+  for (auto &x : data) {
+    meanX += std::get<0>(x);
+    meanY += std::get<1>(x);
+  }
+
+  meanX /= double(data.size());
+  meanY /= double(data.size());
+
+  double stdX = 0.0;
+  double stdY = 0.0;
+
+  for (auto &x : data) {
+    double _x = std::get<0>(x);
+    double _y = std::get<1>(x);
+
+    stdX += (_x - meanX) * (_x - meanX);
+    stdY += (_y - meanY) * (_y - meanY);
+  }
+
+  stdX = sqrt(stdX / double(data.size()));
+  stdY = sqrt(stdY / double(data.size()));
+}
