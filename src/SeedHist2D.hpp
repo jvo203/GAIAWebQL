e547eb8376bc4c4cc1c565a590a0fc421fde5a81
jvo203 <chris.zapart@nao.ac.jp>
2019-12-12 10:45:27 +0900

    modified:   src/SeedHist2D.hpp

--- src/SeedHist2D.hpp
+++ src/SeedHist2D.hpp
@@ -1,3 +1,27 @@
 #pragma once
 
-#include <boost/histogram.hpp>
+#include <tuple>
+#include <vector>
+
+//#include <boost/histogram.hpp>
+
+// CERN ROOT
+#include <TH2.h>
+
+#define BURN_IN 10000
+
+class SeedH2 {
+public:
+  SeedH2() {
+    hist = NULL;
+    data.reserve(BURN_IN);
+  }
+  ~SeedH2(){};
+
+public:
+  void update(float _x, float _y);
+
+private:
+  std::vector<std::tuple<float, float>> data;
+  TH2 *hist;
+};
