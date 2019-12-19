deb86323c7bfd7bfbe2db6ea178947a04ea3dc13
jvo203 <chris.zapart@nao.ac.jp>
2019-12-19 11:08:48 +0900

    modified:   src/SeedHist2D.*

--- src/SeedHist2D.cpp
+++ src/SeedHist2D.cpp
@@ -4,6 +4,41 @@
 
 #include "SeedHist2D.hpp"
 
+SeedH2::SeedH2() {
+  title = "_x <=> _y";
+  init_done = false;
+  data.reserve(BURN_IN);
+  x_min = -FLT_MAX;
+  x_max = FLT_MAX;
+  y_min = -FLT_MAX;
+  y_max = FLT_MAX;
+
+  // allocate the bin memory
+  bin_data = (uint64_t **)malloc(sizeof(uint64_t *) * NBINS);
+
+  if (bin_data != NULL)
+    for (int i = 0; i < NBINS; i++) {
+      bin_data[i] = (uint64_t *)malloc(sizeof(uint64_t) * NBINS);
+
+      if (bin_data[i] != NULL)
+        memset(bin_data[i], 0, NBINS * sizeof(uint64_t));
+      else
+        printf("error allocating bin_data[%d]\n", i);
+    }
+
+  printf("created a SeedH2::%s\n", title.c_str());
+}
+SeedH2::~SeedH2() {
+  printf("%s x_min: %f x_max: %f; y_min: %f, y_max: %f\n", title.c_str(), x_min,
+         x_max, y_min, y_max);
+
+  // deallocate the bins
+  if (bin_data != NULL)
+    for (int i = 0; i < NBINS; i++)
+      if (bin_data[i] != NULL)
+        free(bin_data[i]);
+};
+
 void SeedH2::update(float _x, float _y) {
   if (!init_done)
     data.push_back(std::make_tuple(_x, _y));
--- src/SeedHist2D.hpp
+++ src/SeedHist2D.hpp
@@ -14,45 +14,14 @@ using namespace boost::histogram;*/
 
 class SeedH2 {
 public:
-  SeedH2() {
-    title = "_x <=> _y";
-    init_done = false;
-    data.reserve(BURN_IN);
-    x_min = -FLT_MAX;
-    x_max = FLT_MAX;
-    y_min = -FLT_MAX;
-    y_max = FLT_MAX;
-
-    // allocate the bin memory
-    bin_data = (uint64_t **)malloc(sizeof(uint64_t *) * NBINS);
-
-    if (bin_data != NULL)
-      for (int i = 0; i < NBINS; i++) {
-        bin_data[i] = (uint64_t *)malloc(sizeof(uint64_t) * NBINS);
-
-        if (bin_data[i] != NULL)
-          memset(bin_data[i], 0, NBINS * sizeof(uint64_t));
-        else
-          printf("error allocating bin_data[%d]\n", i);
-      }
-
-    printf("created a SeedH2::%s\n", title.c_str());
-  }
-  ~SeedH2() {
-    printf("%s x_min: %f x_max: %f; y_min: %f, y_max: %f\n", title.c_str(),
-           x_min, x_max, y_min, y_max);
-
-    // deallocate the bins
-    if (bin_data != NULL)
-      for (int i = 0; i < NBINS; i++)
-        if (bin_data[i] != NULL)
-          free(bin_data[i]);
-  };
+  SeedH2();
+  ~SeedH2();
 
 public:
   void set_title(std::string _title) { title = _title; };
   void update(float _x, float _y);
   void flush();
+  void save(std::string uuid, std::string type);
 
 private:
   void fill(float _x, float _y);
