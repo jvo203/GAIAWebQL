a58508ffc7d529099dd9c59feb16954d93fe3dd8
jvo203 <chris.zapart@nao.ac.jp>
2020-01-22 14:55:58 +0900

    modified:   src/SeedHist3D.hpp

--- src/SeedHist3D.hpp
+++ src/SeedHist3D.hpp
@@ -6,39 +6,36 @@
 #include <vector>
 
 // CERN ROOT
-#include <TH2.h>
+#include <TH3.h>
 
 #define BURN_IN 1000000
 #define NBINS 600
 
-class SeedH2 {
+class SeedH3 {
 public:
-  SeedH2(bool _invert = false);
-  ~SeedH2();
+  SeedH3(bool _invert = false);
+  ~SeedH3();
 
 public:
   void set_title(std::string _name, std::string _title, std::string _x_axis,
-                 std::string _y_axis) {
+                 std::string _y_axis, std::string _z_axis) {
     name = _name;
     title = _title;
     x_title = _x_axis;
     y_title = _y_axis;
+    z_title = _z_axis;
   };
-  void update(float _x, float _y);
+  void update(float _x, float _y, float _z);
   void flush();
-  void save(std::string uuid, std::string docs_root, std::string type);
   void export_root(std::string uuid, std::string docs_root, std::string type);
 
 private:
-  void ReverseYAxis(TH1 *h);
-  void ReverseYData(TH2 *h);
-
-private:
-  std::string name, title, x_title, y_title;
+  std::string name, title, x_title, y_title, z_title;
   double x_min, x_max;
   double y_min, y_max;
-  std::vector<std::tuple<float, float>> data;
+  double z_min, z_max;
+  std::vector<std::tuple<float, float, float>> data;
   bool init_done;
   bool invert;
-  TH2 *_hist;
+  TH3 *_hist;
 };
